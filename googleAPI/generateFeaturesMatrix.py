f02db424088715c4a8742955dd093841c94db8d9
Lucas <lrsmello.lucas@gmail.com>
2024-07-23 00:09:08 +0100

    distance_matrix_results

--- googleAPI/generateFeaturesMatrix.py
+++ googleAPI/generateFeaturesMatrix.py
@@ -1,55 +1,78 @@
-import requests
-import json
-import itertools
+import googlemaps
+import pandas as pd
+import time
 
-def get_distance_matrix(origins, destinations, api_key, mode='transit'):
-    base_url = "https://maps.googleapis.com/maps/api/distancematrix/json"
-    
-    params = {
-        'origins': '|'.join(origins),
-        'destinations': '|'.join(destinations),
-        'key': api_key,
-        'mode': mode,
-        'units': 'metric',
-        'transit_routing_preference': 'fewer_transfers'  # Adicional para transporte público, se necessário
-    }
-    
-    response = requests.get(base_url, params=params)
-    
-    if response.status_code == 200:
-        return response.json()
-    else:
-        response.raise_for_status()
+API_KEY = 'AIzaSyAxTNoH5-2ftR84_qLfWnTb4HWU4VjsMMc'
 
-# Função para ler o arquivo JSON
-def read_locations_from_json(file_path):
-    with open(file_path, 'r') as file:
-        return json.load(file)
+gmaps = googlemaps.Client(key=API_KEY)
 
-# Exemplo de uso
-api_key = 'AIzaSyAxTNoH5-2ftR84_qLfWnTb4HWU4VjsMMc'
-file_path = 'data/boroughs.json'
+# Lista de boroughs
+boroughs = [
+    {"ID": 1, "name": "City of London", "latitude": 51.5155, "longitude": -0.0922},
+    {"ID": 2, "name": "Barking and Dagenham", "latitude": 51.5365, "longitude": 0.1272},
+    {"ID": 3, "name": "Barnet", "latitude": 51.6252, "longitude": -0.1517},
+    {"ID": 4, "name": "Bexley", "latitude": 51.4557, "longitude": 0.1504},
+    {"ID": 5, "name": "Brent", "latitude": 51.5588, "longitude": -0.2817},
+    {"ID": 6, "name": "Bromley", "latitude": 51.4036, "longitude": 0.0196},
+    {"ID": 7, "name": "Camden", "latitude": 51.5450, "longitude": -0.1622},
+    {"ID": 8, "name": "Croydon", "latitude": 51.3723, "longitude": -0.1091},
+    {"ID": 9, "name": "Ealing", "latitude": 51.5111, "longitude": -0.3084},
+    {"ID": 10, "name": "Enfield", "latitude": 51.6523, "longitude": -0.0815},
+    {"ID": 11, "name": "Greenwich", "latitude": 51.4821, "longitude": 0.0050},
+    {"ID": 12, "name": "Hackney", "latitude": 51.5456, "longitude": -0.0552},
+    {"ID": 13, "name": "Hammersmith and Fulham", "latitude": 51.4927, "longitude": -0.2339},
+    {"ID": 14, "name": "Haringey", "latitude": 51.5908, "longitude": -0.1117},
+    {"ID": 15, "name": "Harrow", "latitude": 51.5788, "longitude": -0.3339},
+    {"ID": 16, "name": "Havering", "latitude": 51.5742, "longitude": 0.1831},
+    {"ID": 17, "name": "Hillingdon", "latitude": 51.5333, "longitude": -0.4532},
+    {"ID": 18, "name": "Hounslow", "latitude": 51.4672, "longitude": -0.3617},
+    {"ID": 19, "name": "Islington", "latitude": 51.5380, "longitude": -0.1030},
+    {"ID": 20, "name": "Kensington and Chelsea", "latitude": 51.4940, "longitude": -0.1900},
+    {"ID": 21, "name": "Kingston upon Thames", "latitude": 51.4123, "longitude": -0.3005},
+    {"ID": 22, "name": "Lambeth", "latitude": 51.4576, "longitude": -0.1177},
+    {"ID": 23, "name": "Lewisham", "latitude": 51.4452, "longitude": -0.0209},
+    {"ID": 24, "name": "Merton", "latitude": 51.4030, "longitude": -0.1947},
+    {"ID": 25, "name": "Newham", "latitude": 51.5071, "longitude": 0.0462},
+    {"ID": 26, "name": "Redbridge", "latitude": 51.5755, "longitude": 0.0851},
+    {"ID": 27, "name": "Richmond upon Thames", "latitude": 51.4515, "longitude": -0.3346},
+    {"ID": 28, "name": "Southwark", "latitude": 51.5035, "longitude": -0.0804},
+    {"ID": 29, "name": "Sutton", "latitude": 51.3618, "longitude": -0.1945},
+    {"ID": 30, "name": "Tower Hamlets", "latitude": 51.5099, "longitude": -0.0204},
+    {"ID": 31, "name": "Waltham Forest", "latitude": 51.5898, "longitude": -0.0195},
+    {"ID": 32, "name": "Wandsworth", "latitude": 51.4565, "longitude": -0.1910},
+    {"ID": 33, "name": "Westminster", "latitude": 51.4975, "longitude": -0.1372}
+]
 
-# Ler locais do arquivo JSON
-locations = read_locations_from_json(file_path)
+# Função para obter a distância, duração e tarifa
+def get_distance_duration_fare(origin, destination):
+    response = gmaps.distance_matrix(origins=origin, destinations=destination, mode='transit')
+    element = response['rows'][0]['elements'][0]
+    distance = element.get('distance', {}).get('value', 'N/A')
+    duration = element.get('duration', {}).get('value', 'N/A')
+    fare = element.get('fare', {}).get('value', 'N/A')
+    return distance, duration, fare
+# Lista para armazenar os resultados
+results = []
+# Iterando sobre os boroughs para obter informações
+for origin_borough in boroughs:
+    origin = f"{origin_borough['latitude']},{origin_borough['longitude']}"
+    for destination_borough in boroughs:
+        if origin_borough['ID'] != destination_borough['ID']:
+            destination = f"{destination_borough['latitude']},{destination_borough['longitude']}"
+            distance, duration, fare = get_distance_duration_fare(origin, destination)
+            results.append({
+                'origin_ID': origin_borough['ID'],
+                'destination_ID': destination_borough['ID'],
+                'distance': distance,
+                'duration': duration,
+                'fare': fare
+            })
+            
+# Convertendo os resultados para um DataFrame
+results_df = pd.DataFrame(results)
 
-# Gerar todas as combinações possíveis de origens e destinos
-all_combinations = list(itertools.product(locations, repeat=2))
+# Salvando os resultados em um arquivo JSON
+results_df.to_json('data/distance_matrix_results.json', orient='records', lines=True)
 
-all_results = []
-
-try:
-    for combo in all_combinations:
-        origin = f"{combo[0]['latitude']},{combo[0]['longitude']}"
-        destination = f"{combo[1]['latitude']},{combo[1]['longitude']}"
-        origins = [origin]
-        destinations = [destination]
-        result = get_distance_matrix(origins, destinations, api_key, mode='transit')
-        all_results.append(result)
-        print(result)
-except requests.exceptions.RequestException as e:
-    print(f"Erro ao realizar a solicitação: {e}")
-
-# Processar e imprimir resultados
-for result in all_results:
-    print(result)
+# Exibindo o DataFrame
+print(results_df)
