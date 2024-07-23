2d858cebb3e5d47eb9ec60648ae247ed5fb21ec0
Taís Lemos <taisplemos@gmail.com>
2024-07-23 16:14:56 +0100

    Adjusts in front end

--- static/ranking.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    fetch('responsejson')
-        .then(response => response.json())
-        .then(data => {
-            const dataDisplay = document.getElementById("dataDisplay");
-            return(dataDisplay)
-        }
-    )
-})
